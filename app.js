3a63e1df6a3c480ca8850fc4c5e4f6ea9e75f603
london-newyork <cactus1272004@yahoo.co.jp>
2022-06-01 13:20:30 +0900

    (WIP)POSTのテスト - express.Routerを使わないバージョン - サーバー起動してcurl叩くがconsoleにメッセージは表示されない状態

--- app.js
+++ app.js
@@ -3,10 +3,32 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3003;
 
-const mysql = require('mysql2');
-const connection = mysql.createConnection(process.env.DATABASE_URL);
+app.use(express.json())
 
-connection.connect();
+const router = express.Router()
+
+const mysql = require('mysql2');
+// const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+const connection = mysql.createConnection({
+    // host: `localhost/${port}`,
+    host: `localhost`,
+    user: 'root',
+    password: '[事前準備で設定したrootユーザのパスワード]',
+    database: 'tatoeba-db',
+    // process.env.DATABASE_URL
+});
+
+
+// connection.connect();
+
+connection.connect((err) => {
+    if (err) {
+        console.log('error connecting: ' + err.stack);
+        return
+    }
+    console.log('success');
+});
 
 app.get('/', (req, res) => {
     connection.query('SELECT * FROM users', function (err, rows, fields) {
@@ -15,6 +37,13 @@ app.get('/', (req, res) => {
         res.send(rows);
     });
 });
+
+
+app.post('/', function (req, res) {
+    console.log(req.body);
+    res.send('Got a POST request')
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
