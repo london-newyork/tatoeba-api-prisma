cb5caab8697f6859578572dcfaa6cd4d69f57df1
YI <57102971+london-newyork@users.noreply.github.com>
2022-07-13 17:26:49 +0900

    #13 [WIP] ログイン失敗時の動作の実装

--- src/route/A_Router.ts
+++ src/route/A_Router.ts
@@ -6,10 +6,15 @@ import passport from '../lib/security';
 const router = express.Router();
 
 router.post(
-  'login',
-  passport.authenticate('local', { session: false }),
+  '/login',
+  passport.authenticate('local', {
+    session: false,
+    successReturnToOrRedirect: '/',
+    failureRedirect: '/login',
+    failureMessage: true,
+  }),
   (req, res, next) => {
-    // 1 jwtのtokenを作成
+    // 1 jwtのtokenを作成 passwordはどうなるのか=>payloadには含められないはず
     const email = req.body.email;
     const payload = { email: req.body.email };
     const token = jwt.sign(
