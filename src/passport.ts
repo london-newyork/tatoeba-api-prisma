2f917cad3c584ffe033533ccb319d8ea5b078b91
YI <57102971+london-newyork@users.noreply.github.com>
2022-08-31 00:23:56 +0900

    #28 bcyptのエラーが起きていたのでrequireに戻した。

--- src/passport.ts
+++ src/passport.ts
@@ -3,7 +3,7 @@ import { prisma } from '../src/prisma';
 import { Strategy as StrategyLocal } from 'passport-local';
 import { ExtractJwt, Strategy as StrategyJWT } from 'passport-jwt';
 
-import bcrypt from 'bcrypt';
+const bcrypt = require('bcrypt');
 
 passport.use(
   new StrategyLocal(
@@ -11,14 +11,14 @@ passport.use(
     async (email: string, password: string, done) => {
       const user = await prisma.user.findUnique({ where: { email } });
       if (!user) {
-        return done(new Error('ログイン情報が正しくありません。'), null);
+        return done(new Error('ログイン情報が正しくありません。1'), null);
       }
       const isOK = await bcrypt.compare(password, user?.password);
 
       if (isOK) {
         return done(null, user); //ログイン成功時はfalseの部分がユーザー情報に書き換わる。失敗時はfalse
       } else {
-        return done(new Error('ログイン情報が正しくありません。'), null);
+        return done(new Error('ログイン情報が正しくありません。2'), null);
       }
     }
   )
