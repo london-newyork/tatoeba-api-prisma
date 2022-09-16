6631cb61dd09cf8d5db4239ca0d469d2d64ad1e3
YI <57102971+london-newyork@users.noreply.github.com>
2022-09-16 23:37:49 +0900

    #32 [WIP] GCSから取得した画像をフロント側へ送る

--- src/route/UserRouter.ts
+++ src/route/UserRouter.ts
@@ -1,14 +1,13 @@
 import express from 'express';
-import multer from 'multer';
 
 import passport from 'passport';
 import { prisma } from '../prisma';
-import { Storage } from '@google-cloud/storage';
-import sharp from 'sharp';
 
 import { RequestUser } from '../@types/express';
 import UserTatoeRouter from '../route/UserTatoeRouter';
 import fs from 'fs/promises';
+import { readFileSync, promises as fsPromises } from 'fs';
+import { upload, bucketName, googleStorage } from '../googleCloudStorage';
 
 const router = express.Router();
 
@@ -90,17 +89,43 @@ router.put(
 );
 
 // TODO GET: /users /:id/profile_image
+router.get(
+  '/:id/profile_image',
+  passport.authenticate('jwt', { session: false }),
+  // upload.single('image'),
+  // fetch(---, {body: {image: File }})
+  async (req: express.Request, res: express.Response, next) => {
+    const id = req.params.id;
+    const userId = (req.user as RequestUser)?.id;
+    const file = req.file;
 
-// Multerを使ってファイル名を書き換える
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
-const upload = multer({ storage: storage });
+    if (userId === id) {
+      if (file && bucketName) {
+        const main = async () => {
+          // GCSからの読み込み
+          await googleStorage
+            .bucket(bucketName as string)
+            .getFiles({ prefix: '/' })
+            // .getFiles({ prefix: `/${userId}` }) // putで作成したディレクトリ名とuserId指定
+            .then((res) => {
+              console.log('Success');
+            })
+            .catch((err) => {
+              console.error('ERROR:', err);
+            });
+        };
+        main();
+        // フロントへ画像を送る
+        await fs.readFile('illust350.png').then((data) => {
+          res.type('png');
+          res.send(data);
+        });
+      } else throw 'There are no file and bucketName';
+    } else throw 'Different user';
+    res.json();
+    next();
+  }
+);
 
 // profile_image file登録
 router.put(
@@ -108,29 +133,20 @@ router.put(
   passport.authenticate('jwt', { session: false }),
   upload.single('image'),
   // fetch(---, {body: {image: File }})
-  async (req: express.Request, res: express.Response) => {
+  async (req: express.Request, res: express.Response, next) => {
     const id = req.params.id;
     const userId = (req.user as RequestUser)?.id;
     const file = req.file;
 
-    const thisProjectId = process.env.GCS_PROJECT_ID;
-    const keyFilename = process.env.GCS_SERVICE_KEY_PATH;
-
-    // 任意の名前で作成したバケット名
-    const bucketName = process.env.GCS_BUCKET_NAME;
-
-    // バケットの取得
-    const googleStorage = new Storage({
-      projectId: thisProjectId,
-      keyFilename: keyFilename,
-    });
+    // TODO userごとにディレクトリを作る必要がある
+    // (bucketName)/user_images/userId/..png
 
     if (userId === id) {
       if (file && bucketName) {
         const main = async () => {
           try {
-            await googleStorage
-              .bucket(bucketName)
+            const data = await googleStorage
+              .bucket(bucketName as string)
               .upload(`${file.path}`, { gzip: true })
               .then((res) => {
                 // 公開状態にする場合
@@ -141,14 +157,22 @@ router.put(
               .catch((err) => {
                 console.error('ERROR:', err);
               });
+            // TODO データとしてフロントに返してフロントで画像更新したい
+
+            res.json({ data });
+            console.log('data', data);
           } finally {
             await fs.unlink(file.path);
             console.log('File has been deleted');
           }
         };
         main();
-      } else throw 'There are no file and bucketName';
-    } else throw 'Different user';
+      } else {
+        console.log('There are no file and bucketName');
+        next();
+      }
+    }
+    console.log('Different User');
   }
 );
 
