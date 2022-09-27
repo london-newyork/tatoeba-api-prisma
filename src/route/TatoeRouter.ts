1c7f62fc115f3748b5cce48a7451f89474518894
YI <57102971+london-newyork@users.noreply.github.com>
2022-09-27 17:48:13 +0900

    #38 formメソッドを使って他の例えのコンテンツ登録と同時に説明画像も登録したいので、 例え登録のメソッドを修正した。 - 以前作成していた説明画像単独のメソッドは使わないので一旦コメントアウト

--- src/route/TatoeRouter.ts
+++ src/route/TatoeRouter.ts
@@ -3,10 +3,7 @@ import passport from 'passport';
 import { RequestUser } from '../@types/express';
 import { prisma } from '../prisma';
 import { DateFormat, dateFormat, formatDate, FormattedDate } from '../date';
-
-import { createReadStream } from 'fs';
 import { unlink } from 'fs/promises';
-import path from 'path';
 import { upload, bucketName, googleStorage } from '../googleCloudStorage';
 const router = express.Router();
 
@@ -35,10 +32,31 @@ router.get('/:tId', async (req: express.Request, res: express.Response) => {
 router.post(
   '/',
   passport.authenticate('jwt', { session: false }),
+  upload.single('image'),
   async (req: express.Request, res: express.Response) => {
     const userId = (req.user as RequestUser)?.id;
     const { tId, title, shortParaphrase, description } = req.body;
+    const file = req.file;
 
+    if (file && bucketName) {
+      try {
+        const data = await googleStorage
+          .bucket(bucketName as string)
+          .upload(`${file.path}`, {
+            gzip: true,
+            destination: `tatoe_images/${tId}`,
+          });
+
+        res.json({ data });
+        console.log('data', data);
+      } finally {
+        await unlink(file.path);
+        console.log('File has been deleted');
+      }
+    } else {
+      console.log('There are no file and bucketName');
+    }
+    // TODO 作成したimageUrlカラムにもURLを格納したい
     const tatoe = await prisma.tatoe.create({
       data: {
         userId,
@@ -105,70 +123,71 @@ router.delete(
   }
 );
 
-// 説明画像
-router.get(
-  '/:id/explanation_image',
-  async (req: express.Request, res: express.Response, next) => {
-    const id = req.params.id; // tId
-    const userId = (req.user as RequestUser)?.id;
-    console.log('====GET tId???', id);
-    console.log('====GET USER ID???', userId); // false
-    const file = googleStorage
-      .bucket(bucketName as string)
-      .file(`tatoe_images/${id}`);
+// // 説明画像
+// router.get(
+//   '/:id/explanation_image',
+//   async (req: express.Request, res: express.Response, next) => {
+//     const id = req.params.id; // tId
+//     const userId = (req.user as RequestUser)?.id;
+//     console.log('====GET tId???', id);
+//     console.log('====GET USER ID???', userId); // false
+//     const file = googleStorage
+//       .bucket(bucketName as string)
+//       .file(`tatoe_images/${id}`);
 
-    const [exists] = await file.exists();
+//     const [exists] = await file.exists();
 
-    console.log('====GET EXISTS??', exists); // false
-    if (exists) {
-      const stream = file.createReadStream();
-      stream.on('error', (error) => {
-        console.log(`${error}`);
-        res.statusCode = 500;
-        res.end('500 error');
-      });
-      stream.pipe(res);
-    }
-    // デフォルト画像はフロント側CSSで用意されているのでいらない
-  }
-);
+//     console.log('====GET EXISTS??', exists); // false
+//     if (exists) {
+//       const stream = file.createReadStream();
+//       stream.on('error', (error) => {
+//         console.log(`${error}`);
+//         res.statusCode = 500;
+//         res.end('500 error');
+//       });
+//       stream.pipe(res);
+//     }
+//     // デフォルト画像はフロント側CSSで用意されているのでいらない
+//   }
+// );
 
-router.put(
-  '/:id/explanation_image',
-  passport.authenticate('jwt', { session: false }),
-  upload.single('image'),
-  async (req: express.Request, res: express.Response, next) => {
-    const id = req.params.id; // tId
-    const userId = (req.user as RequestUser)?.id;
-    const file = req.file;
-    console.log('======PUT tId', id);
-    console.log('======PUT USER ID', userId);
-    console.log('===EXPLANATION IMAGE', file);
+// router.put(
+//   '/:id/explanation_image',
+//   passport.authenticate('jwt', { session: false }),
+//   upload.single('image'),
+//   async (req: express.Request, res: express.Response, next) => {
+//     const id = req.params.id; // tId
+//     const userId = (req.user as RequestUser)?.id;
+//     const file = req.file;
+//     console.log('======PUT tId', id);
+//     console.log('======PUT USER ID', userId);
+//     console.log('===EXPLANATION IMAGE', file);
 
-    // バケットのパスにuserIdいれたほうがいいか検討中
-    // uploadsというディレクトリにフロントから画像が来るが、userIdも同じディレクトリになっているので、大丈夫か
-    if (file && bucketName) {
-      try {
-        const data = await googleStorage
-          .bucket(bucketName as string)
-          .upload(`${file.path}`, {
-            gzip: true,
-            destination: `tatoe_images/${id}`,
-          });
+//     // バケットのパスにuserIdいれたほうがいいか検討中
+//     // uploadsというディレクトリにフロントから画像が来るが、userIdも同じディレクトリになっているので、大丈夫か
+//     if (file && bucketName) {
+//       try {
+//         const data = await googleStorage
+//           .bucket(bucketName as string)
+//           .upload(`${file.path}`, {
+//             gzip: true,
+//             destination: `tatoe_images/${id}`,
+//           });
 
-        res.json({ data });
-        console.log('data', data);
-      } finally {
-        await unlink(file.path);
-        console.log('File has been deleted');
-      }
-    } else {
-      console.log('There are no file and bucketName');
-      next();
-    }
-  }
-);
+//         res.json({ data });
+//         console.log('data', data);
+//       } finally {
+//         await unlink(file.path);
+//         console.log('File has been deleted');
+//       }
+//     } else {
+//       console.log('There are no file and bucketName');
+//       next();
+//     }
+//   }
+// );
 
+// TODO tatoeに登録した説明画像を取り出すためパスを変更する必要がある
 router.delete(
   '/:id/explanation_image',
   passport.authenticate('jwt', { session: false }),
